3ae004b7e3bb07dc744e97f1735ae569e0db137b
candra <candraherdiansyah14@gmail.com>
2025-12-26 08:47:33 +0700

    day 14

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -1,10 +1,22 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'product_name',
+        'price',
+        'quantity',
+    ];
+
+    // ==================== RELATIONSHIPS ====================
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
 }
