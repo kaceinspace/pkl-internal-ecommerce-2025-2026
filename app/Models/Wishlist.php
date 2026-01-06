4086964b0c121babc5f7fb35b8dc8a46f67006a8
candra <candraherdiansyah14@gmail.com>
2026-01-06 13:37:08 +0700

    day18-20

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -19,7 +19,7 @@ class Wishlist extends Model
             ->withTimestamps(); // Agar created_at/updated_at di pivot terisi
     }
 
-// Helper untuk cek apakah user sudah wishlist produk tertentu
+    // Helper untuk cek apakah user sudah wishlist produk tertentu
     public function hasInWishlist(Product $product)
     {
         return $this->wishlists()->where('product_id', $product->id)->exists();
